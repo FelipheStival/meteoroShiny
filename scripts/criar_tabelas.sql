671179ba7564dee27ea77889cd847d62702ca728
FelipheStival <feliphestivalv.g@gmail.com>
2021-08-25 10:50:40 -0300

    Atualizando scritps

--- scripts/criar_tabelas.sql
+++ scripts/criar_tabelas.sql
@@ -21,6 +21,8 @@ CREATE TABLE public.cidades
 (
     id bigserial NOT NULL,
     nome text NOT NULL,
+    latitude double precision NOT NULL,
+    longitude double precision NOT NULL,
     id_estado bigint NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT "FK_cidades_estados" FOREIGN KEY (id_estado)
@@ -37,15 +39,15 @@ CREATE TABLE public.clima
 (
     id bigserial NOT NULL,
     data date NOT NULL,
-    "Tmax" numeric NOT NULL,
-    "Tmin" numeric NOT NULL,
-    "Tmed" numeric NOT NULL,
-    "Urmed" numeric NOT NULL,
-    "Vento" numeric NOT NULL,
-    "Vtmax" numeric NOT NULL,
-    "Rad" numeric NOT NULL,
-    "Precip" numeric NOT NULL,
-    "Tsolo" numeric NOT NULL,
+    Tmax numeric NOT NULL,
+    Tmin numeric NOT NULL,
+    Tmed numeric NOT NULL,
+    Urmed numeric NOT NULL,
+    Vento numeric NOT NULL,
+    Vtmax numeric NOT NULL,
+    Rad numeric NOT NULL,
+    Precip numeric NOT NULL,
+    Tsolo numeric NOT NULL,
     id_cidade bigint NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT "FK_clima_cidades" FOREIGN KEY (id_cidade)
