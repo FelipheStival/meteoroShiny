76b484b35f3284a9ac0982ea13a2cbf62ad748f7
FelipheStival <feliphestivalv.g@gmail.com>
2022-10-09 14:29:33 -0300

    Atualizando scripts

--- app/cor/globals.R
+++ app/cor/globals.R
@@ -2,23 +2,13 @@
 APP_NAME = "INMET"
 APP_LANGUAGE = "pt"
 
-#DB_HOST = "192.168.144.13"
-#DB_PORT = "5432"
-#DB_DATABASE = "meteoro"
-#DOENCA_DB_DATABASE = "ensaios"
-#DB_USERNAME = "meteoro"
-#DB_PASSWORD = "meteoro"
-#DB_DRIVER = "PostgreSQL"
-
-
-DB_HOST = "localhost"
+DB_HOST = "192.168.144.13"
 DB_PORT = "5432"
 DB_DATABASE = "meteoro"
 DOENCA_DB_DATABASE = "ensaios"
-DB_USERNAME = "METEORO"
-DB_PASSWORD = "METEORO"
+DB_USERNAME = "meteoro"
+DB_PASSWORD = "meteoro"
 DB_DRIVER = "PostgreSQL"
-
 
 # Acesso
 credenciais = data.frame(
