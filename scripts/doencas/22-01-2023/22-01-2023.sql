33e4faaab71d252cf848783c2d2a0c5d22b547fc
felipheStival <feliphestivalv.g@gmail.com>
2023-03-19 22:35:43 -0300

    Removendo pasta doenças

--- scripts/doencas/22-01-2023/22-01-2023.sql
+++
@@ -1,6 +0,0 @@
-/* removendo coluna id_cultura */
-ALTER TABLE ensaios
-DROP COLUMN categoria_id;
-
-/* Apagando tabela categoria */
-DROP TABLE categoria;
--- scripts/doencas/GRUD-tabelas/remove_tabelas.sql
+++
@@ -1,7 +0,0 @@
-/*------------------------------------------------
-* Script para apagar as tabelas
-* Author: Feliphe Stival Valadares Guiliani
-* Versao: 0.1
-*/------------------------------------------------
-
-DROP TABLE ensaios,genotipos,tipos_de_graos,locais,cidades,estados;
